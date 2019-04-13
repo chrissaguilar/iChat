6eeeb520f76991151e07bdc9016b2e5da0e4c857
Chris Aguilar <Chris@Chriss-MacBook-Pro.local>
2019-04-13 14:37:35 -0600

    Added Message module and linked Message to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
-  validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 20 }
+  validates_uniqueness_of :username, :case_sensitive => false
+  validates :username, presence: true, length: { minimum: 3, maximum: 20 }
+  has_many :messages
   has_secure_password
 end
